bd290f7a4ac4f7ec1d302f031e54508d8c49a0c7
SHUBHAM WADJE <shubhamwadje2005@gmail.com>
2025-03-21 15:33:19 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
 const { protectedRoute } = require("./middleware/blog.middlewar")
+const path = require("path");
 require("dotenv").config()
 
 const app = express()
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(express.static("dist"));
+app.use(cors({ origin: "https://task-home.onrender.com", credentials: true }))
 app.use(cookieParser())
 
 app.use("/api/auth", require("./routers/user.route"))
 app.use("/api/blog", protectedRoute, require("./routers/blog.route"))
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "resource not found" })
+    // res.status(404).json({ message: "resource not found" })
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+
 })
 
 mongoose.connect(process.env.MONGO_URL)
